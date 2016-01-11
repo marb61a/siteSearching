6e18c23438e5d6c279a4a8eff80c89289cb28847
marb61a <marb61a@gmail.com>
2016-01-11 10:46:13 +0000

    added post route to index.js file

--- sitesearchengine/routes/index.js
+++ sitesearchengine/routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  var searchText = req.body.searchText;
+  var searchType = req.body.searchType;
+
+  if(searchType == 'website'){
+  	Website.searchWebsites(searchText, function(err, websites){
+  		if(err){
+  			res.send(err);
+  		}
+  		var model = {
+  			websites: websites.results
+  		}
+
+  		res.render('website_results',model);
+  	});
+  } else if(searchType == 'news'){
+
+  } else {
+  	res.send('Choose website or news');
+  }
+});
+
 module.exports = router;
