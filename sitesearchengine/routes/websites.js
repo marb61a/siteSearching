f9a07d888fee8cb16263fb6445e07ff4bcb250b5
marb61a <marb61a@gmail.com>
2016-01-11 10:41:04 +0000

    add get and post routes to websites.js file

--- sitesearchengine/routes/websites.js
+++ sitesearchengine/routes/websites.js
@@ -5,4 +5,26 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/add', function(req, res, next) {
+  Category.getCategories(function(err, categories){
+  	res.render('add_website',{
+  		categories: categories
+  	});
+  });
+});
+
+router.post('/add', function(req, res, next) {
+	var website = new Website();
+	website.title = req.body.title;
+	website.url = req.body.url;
+	website.description = req.body.description;
+
+  Website.addWebsite(website, function(err, website){
+  	if(err){
+  		res.send(err);
+  	}
+  	res.redirect('/');
+  });
+});
+
 module.exports = router;
