3f2a4ce60a5b44705c084c44b54116d8508e5530
marb61a <marb61a@gmail.com>
2016-01-15 11:40:32 +0000

    updateded articles model

--- sitesearchengine/models/article.js
+++ sitesearchengine/models/article.js
@@ -27,3 +27,23 @@ var articleSchema = mongoose.Schema({
 		default: Date.now
 	}
 });
+
+articleSchema.plugin(searchPlugin, {
+    fields: ['title', 'description', 'url']
+  });
+
+var Article = module.exports = mongoose.model('Article', articleSchema);
+
+// Search Articles
+module.exports.searchArticles= function(searchText, callback){
+	Article.search(searchText, {title: 1, description: 1, url: 1},{
+		conditions:{title:{$exists: true}, description:{$exists: true}, url: {$exists
+			:true}},
+			sort: {title: 1},
+			limit: 50
+	}, callback);
+}
+
+module.exports.addArticle = function(article, callback){
+	Article.create(article, callback);
+}
